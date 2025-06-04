25ac8986c31835f1a19ee129e20c0b1eb7c8d847
MrShaazAli110 <152193116+MrShaazAli110@users.noreply.github.com>
2025-06-05 01:26:24 +0530

    VideoToAudio.py

--- VideoToAudio.py
+++ VideoToAudio.py
@@ -1,10 +1,10 @@
-import moviepy
+import moviepy 
 from tkinter.filedialog import *
 
 vid = askopenfilename()
 video = moviepy.VideoFileClip(vid)
 
 aud = video.audio
-aud.write_audiofile("Ali.mp3")
+aud.write_audiofile("File.mp3")
 
 print("--End--")
